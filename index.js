70b30b92bb9e4983d02582e6ea0783ac33bbcdcc
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-12-02 16:50:37 +0500

    2.6

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
 
 db.sync()
   .then((result) => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 1000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}.`);
     });
