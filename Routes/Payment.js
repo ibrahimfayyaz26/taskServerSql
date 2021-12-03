62e045990ea87463f228f25ffeb6d34284be587d
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-12-03 16:04:54 +0500

    2.51

--- Routes/Payment.js
+++ Routes/Payment.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../config/Multer");
-const {
-  createPayment,
-} = require("../controllers/Payment.controller");
+const { createPayment } = require("../controllers/Payment.controller");
 
-
-router.put("/:id", upload.fields([{ name: 'passport'}, { name: 'idCard' }]),(req, res) => {
-  
-  createPayment(req, res);
-});
+router.put(
+  "/:id",
+  upload.fields([
+    {
+      name: "passport",
+    },
+    {
+      name: "idCard",
+    },
+  ]),
+  cors(),
+  (req, res) => {
+    createPayment(req, res);
+  }
+);
 
 module.exports = router;
